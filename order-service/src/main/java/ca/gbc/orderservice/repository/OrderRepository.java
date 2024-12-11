594bf01faee88f21a4d3f6c4a1054e8e66037c4a
Talalovic <talalovic.9@gmail.com>
2024-12-11 12:27:40 -0500

    Ensure only files under microservice-parent are committed

--- order-service/src/main/java/ca/gbc/orderservice/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package ca.gbc.orderservice.repository;
-
-import ca.gbc.orderservice.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
