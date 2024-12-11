aec9243aeba00f0e3b034bb548805ab22069b6b8
Talalovic <talalovic.9@gmail.com>
2024-12-11 12:30:53 -0500

    Uploaded the new microservice-parent folder

---
+++ microservice-parent/inventory-service/src/main/resources/db/migration/v2__add_inventory.sql
@@ -0,0 +1,8 @@
+INSERT INTO t_inventory (quantity, sku_code)
+VALUES
+    (100, 'SKU001'),
+    (100, 'SKU002'),
+    (100, 'SKU003'),
+    (100, 'SKU004'),
+    (100, 'SKU005');
+
---
+++ microservice-parent/local-docker-file/postgres/init/order/init.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE "order-service";
+GRANT ALL PRIVILEGES ON DATABASE "order-service" TO "admin"
+
+                                 
