e5ab7d474f55dc5710e4ef0286df5b9302b45b32
Talalovic <talalovic.9@gmail.com>
2024-12-11 12:35:39 -0500

    Remove all files to restructure repository

--- inventory-service/src/main/java/ca/gbc/inventoryservice/service/InventoryService.java
+++
@@ -1,6 +0,0 @@
-package ca.gbc.inventoryservice.service;
-
-public interface InventoryService {
-
-    public boolean isInStock(String skuCode, Integer quantity);
-}
--- microservice-parent/order-service/src/main/java/ca/gbc/orderservice/client/InventoryClient.java
+++
@@ -1,9 +0,0 @@
-package ca.gbc.orderservice.client;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.service.annotation.GetExchange;
-
-public interface InventoryClient {
-
-    @GetExchange("/api/inventory")
-    boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
-}
